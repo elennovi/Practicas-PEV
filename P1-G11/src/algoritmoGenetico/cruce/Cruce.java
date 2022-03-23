2fb69795de81c672e05bd6f78cbaafa0b28231a8
Enenita <elennovi@ucm.es>
2022-03-23 18:21:04 +0100

    A ver, probemos

--- P1-G11/src/algoritmoGenetico/cruce/Cruce.java
+++
@@ -1,8 +0,0 @@
-package algoritmoGenetico.cruce;
-
-import algoritmoGenetico.individuos.Individuo;
-
-public abstract class Cruce {
-	
-	public abstract Individuo[] cruzar(Individuo i1, Individuo i2);
-}
