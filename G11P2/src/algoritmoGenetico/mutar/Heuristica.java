613fa1f0f3f1800c7974068647ce2597692170ea
LuciaHeredero <luhere01@ucm.es>
2022-04-02 13:22:00 +0200

    mutaciones

--- G11P2/src/algoritmoGenetico/mutar/Heuristica.java
+++ G11P2/src/algoritmoGenetico/mutar/Heuristica.java
@@ -1,18 +1,63 @@
 package algoritmoGenetico.mutar;
 
+import java.util.Random;
+
 import algoritmoGenetico.individuos.Individuo;
+import algoritmoGenetico.individuos.IndividuoInt;
 
 public class Heuristica extends Mutacion {
+	
+	private static final int n = 3; //Numero de genes a mutar
 
 	public Heuristica(double p) {
 		super(p);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void mutar(Individuo i) {
-		// TODO Auto-generated method stub
+		IndividuoInt indi = (IndividuoInt) i;
 		
+		// Generamos las 3 posiciones a mutar y guardamos el número de vuelo correspondiente
+		Random rand = new Random();
+		int [] pos = new int[n];//Posiciones aleatorias
+		int [] elem = new int[n];//Elemenetos en esas posiciones
+		int[] orden = new int[n];//Orden final, inicializado al actual.
+		for(int j = 0; j < n; j++) {
+			pos[j] = rand.nextInt(i.getL());
+			elem[j] = indi.getAt(pos[j]);
+			orden[j] = elem[j];
+		}
+		
+		System.out.println(elem[0] + " " + elem[1] + " " + elem[2]);
+		
+		
+		IndividuoInt auxiliar = (IndividuoInt) indi.copia();
+		double mejor = auxiliar.getFitness();
+		
+		//Generamos permutaciones
+		for(int j = 1; j < n; j++) {
+			for(int k = 0; k < n && k != j; k++) {
+				for(int l = 0; l < n && l != j && l != k; l++) {
+					//cambiamos el individuo auxiliar para que tenga ese orden y lo evaluamos
+					auxiliar.setAt(pos[0], elem[j]);
+					auxiliar.setAt(pos[1], elem[k]);
+					auxiliar.setAt(pos[2], elem[l]);
+					auxiliar.evaluar();
+					//Si el nuevo individuo es mejor, nos quedamos con él
+					if(auxiliar.getFitness() < mejor) {
+						mejor = auxiliar.getFitness();
+						orden[0] = elem[j];
+						orden[1] = elem[k];
+						orden[2] = elem[l];
+					}
+				}
+			}
+		}
+		
+		
+		//Colocamos el mejor orden 
+		for(int j = 0; j < n; j++)
+			indi.setAt(pos[j], orden[j]);
 	}
 
 }
--- G11P2/src/algoritmoGenetico/mutar/Inversion.java
+++ G11P2/src/algoritmoGenetico/mutar/Inversion.java
@@ -1,18 +1,57 @@
 package algoritmoGenetico.mutar;
 
+import java.util.Random;
+
 import algoritmoGenetico.individuos.Individuo;
+import algoritmoGenetico.individuos.IndividuoInt;
 
 public class Inversion extends Mutacion {
 
 	public Inversion(double p) {
 		super(p);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void mutar(Individuo i) {
-		// TODO Auto-generated method stub
+		IndividuoInt ind = (IndividuoInt) i;
 		
+		//Si probabilisticamente toca mutar
+		if(Math.random() <= super.getProbMutar()) {
+			// Generamos los 2 puntos de inverión
+			Random rand = new Random();
+			int r1 = rand.nextInt(i.getL()), r2 = rand.nextInt(i.getL());
+			int mayor = max(r1, r2), menor= min(r1, r2);
+			
+			//Creamos un array para copiar los elementos que hay que invertir (los copiaremos del último al primero.
+			int [] copia = new int[mayor - menor];
+			int cont = 0;
+			for(int j = mayor; j > menor; j--) {
+				copia[cont] = ind.getAt(j);
+				cont++;
+			}
+			
+			//Invertimos el orden en el individuo
+			cont = 0;
+			for(int j = menor + 1; j <= mayor; j++) {
+				ind.setAt(j, copia[cont]);
+				cont ++;
+			}
+			
+		}		
+	}
+	
+	//Menor de 2 numeros
+	private int min(int r1, int r2) {
+		if(r1 < r2)
+			return r1;
+		return r2;
+	}
+
+	//Mayor de 2 numeros
+	private int max(int r1, int r2) {
+		if(r1 > r2)
+			return r1;
+		return r2;
 	}
 
 }
