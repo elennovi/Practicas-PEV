6e4cf66a6f1feaa746f8dc1403833d673a76fdb0
LuciaHeredero <luhere01@ucm.es>
2022-04-02 21:04:34 +0200

    OX basico y posiciones. Si veo una triste linea de codigo mas hoy me defenestro

--- G11P2/src/algoritmoGenetico/cruce/OXBasico.java
+++ G11P2/src/algoritmoGenetico/cruce/OXBasico.java
@@ -1,14 +1,112 @@
 package algoritmoGenetico.cruce;
 
+import java.util.Random;
+
 import algoritmoGenetico.individuos.Individuo;
 import algoritmoGenetico.individuos.IndividuoInt;
 
 public class OXBasico extends Cruce{
 
-	@Override
-	public Individuo[] cruzar(IndividuoInt i1, IndividuoInt i2) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Individuo[] cruzar(IndividuoInt madre, IndividuoInt padre) {
+		
+		IndividuoInt[] hijos = new IndividuoInt[2];
+		//Creamos copias de los padres para mantener el intervalo 
+		hijos[0] = (IndividuoInt)madre.copia();
+		hijos[1] = (IndividuoInt)padre.copia();
+		
+		//Generamos los 2 puntos de cruce aleatorios y vemos cual es mayor y cual es menor
+		Random rand = new Random();
+		int r1 = rand.nextInt(madre.getL()), r2 = rand.nextInt(madre.getL());
+		int mayor = max(r1, r2), menor= min(r1, r2);
+		
+		//Ponemos un limite del bucle(cambiara desde el mayor hasta el menor+1. Si el menor coincide con el mayor, se hara hasta el mayor
+		int limit = menor + 1;
+		if(menor == padre.getL() - 1)
+			limit = mayor;
+		//Recorremos las posiciones vacías y las completamos con las correspondientes
+		int contPadre = mayor + 1, contHijo = mayor + 1;
+		//Si nos pasamos de la longitud del array
+		if(contPadre >= padre.getL())
+			contPadre = 0;
+		if(contHijo >= hijos[0].getL())
+			contHijo = 0;
+		
+
+		System.out.println(menor+" "+mayor);
+		
+		if(mayor != menor) {
+			while(contHijo != limit) {
+	
+				if(isNotInRange(mayor, menor, padre.getAt(contPadre), hijos[0])) {
+					//Si se puede poner
+					hijos[0].setAt(contHijo, padre.getAt(contPadre));
+					contHijo++;
+					contPadre++;
+				}
+				else 
+					contPadre++;
+				
+				
+				//Si nos pasamos de la longitud del array
+				if(contPadre >= padre.getL())
+					contPadre = 0;
+				if(contHijo >= hijos[0].getL()) 
+					contHijo = 0;
+			
+			}
+			
+			int contMadre = mayor + 1;
+			contHijo = mayor + 1;
+			//Si nos pasamos de la longitud del array
+			if(contMadre >= madre.getL())
+				contMadre = 0;
+			if(contHijo >= hijos[1].getL())
+				contHijo = 0;
+			
+			while(contHijo != limit) {
+				
+				if(isNotInRange(mayor, menor, madre.getAt(contMadre), hijos[1])) {
+					//Si se puede poner
+					hijos[1].setAt(contHijo, madre.getAt(contMadre));
+					contHijo++;
+					contMadre++;
+				}
+				else 
+					contMadre++;
+		
+				//Si nos pasamos de la longitud del array
+				if(contMadre >= madre.getL())
+					contMadre = 0;
+				if(contHijo >= hijos[1].getL())
+					contHijo = 0;
+			}
+		}
+		
+		return hijos;
 	}
+	
+	//Un elemento no está en un array en un rango. 
+	private boolean isNotInRange(int mayor, int menor, int n, IndividuoInt indi) {
+		for(int i = menor + 1; i <= mayor; i++) 
+			if(indi.getAt(i) == n) 
+				return false;
+		return true;
+	}
+
+	//Menor de 2 numeros
+	private int min(int r1, int r2) {
+		if(r1 < r2)
+			return r1;
+		return r2;
+	}
+
+	//Mayor de 2 numeros
+	private int max(int r1, int r2) {
+		if(r1 > r2)
+			return r1;
+		return r2;
+	}
+
 
 }
--- G11P2/src/algoritmoGenetico/cruce/OXPosPrio.java
+++ G11P2/src/algoritmoGenetico/cruce/OXPosPrio.java
@@ -1,14 +1,115 @@
 package algoritmoGenetico.cruce;
 
+import java.util.Random;
+
 import algoritmoGenetico.individuos.Individuo;
 import algoritmoGenetico.individuos.IndividuoInt;
 
 public class OXPosPrio extends Cruce {
+	
+	private static final int n = 3;
 
 	@Override
-	public Individuo[] cruzar(IndividuoInt i1, IndividuoInt i2) {
-		// TODO Auto-generated method stub
-		return null;
+	public Individuo[] cruzar(IndividuoInt madre, IndividuoInt padre) {
+		
+		IndividuoInt[] hijos = new IndividuoInt[2];
+		//Creamos copias de los padres para mantener el intervalo 
+		hijos[0] = (IndividuoInt)madre.copia();
+		hijos[1] = (IndividuoInt)padre.copia();
+		
+		//Generamos los 2 puntos de cruce aleatorios y vemos cual es mayor y cual es menor
+		Random rand = new Random();
+		int[] posPrio = new int[n];
+		for(int i = 0; i < n; i++)
+			posPrio[i] = rand.nextInt(madre.getL());
+		int mayor = max(posPrio);
+		
+		//Recorremos las posiciones vacías y las completamos con las correspondientes
+		
+		int contPadre = mayor + 1, contHijo = mayor + 1;
+		//Si nos pasamos de la longitud del array
+		if(contPadre >= padre.getL())
+			contPadre = 0;
+		if(contHijo >= hijos[0].getL())
+			contHijo = 0;
+		
+		while(contHijo != mayor) {
+			if(isNotPosPrio(contHijo, posPrio)) {
+				if(isNotInRange(posPrio, padre.getAt(contPadre), hijos[0])) {
+					//Si se puede poner
+					hijos[0].setAt(contHijo, padre.getAt(contPadre));
+					contHijo++;
+					contPadre++;
+				}
+				else 
+					contPadre++;
+			}
+			else//Si es posicion prioritaria, pasamos a cambiar la siguiente
+				contHijo++;
+				
+			//Si nos pasamos de la longitud del array
+			if(contPadre >= padre.getL())
+				contPadre = 0;
+			if(contHijo >= hijos[0].getL())
+				contHijo = 0;
+		}
+		
+		int contMadre = mayor + 1;
+		contHijo = mayor + 1;
+		//Si nos pasamos de la longitud del array
+		if(contMadre >= madre.getL())
+			contMadre = 0;
+		if(contHijo >= hijos[1].getL())
+			contHijo = 0;
+		
+		while(contHijo != mayor) {
+			if(isNotPosPrio(contHijo, posPrio)) {
+				if(isNotInRange(posPrio, madre.getAt(contMadre), hijos[1])) {
+					//Si se puede poner
+					hijos[1].setAt(contHijo, madre.getAt(contMadre));
+					contHijo++;
+					contMadre++;
+				}
+				else 
+					contMadre++;
+			}
+			else//Si es posicion prioritaria, pasamos a cambiar la siguiente
+				contHijo++;
+				
+	
+			//Si nos pasamos de la longitud del array
+			if(contMadre >= madre.getL())
+				contMadre = 0;
+			if(contHijo >= hijos[1].getL())
+				contHijo = 0;
+		}
+		
+		
+		return hijos;
+	}
+	
+	private boolean isNotPosPrio(int contHijo, int[] posPrio) {
+		for(int i = 0; i < posPrio.length; i++)
+			if(posPrio[i] == contHijo)
+				return false;
+		return true;
+	}
+
+	//Un elemento no está en un array en ciertas posiciones. 
+	private boolean isNotInRange(int[] lpos, int n, IndividuoInt indi) {
+		for(int i = 0; i < lpos.length; i++) 
+			if(indi.getAt(lpos[i]) == n) 
+				return false;
+		return true;
+	}
+
+	//Mayor de 2 numeros
+	private int max(int[] list) {
+		int maxim = 0;
+		for(int i : list)
+			if(i > maxim)
+				maxim = i;
+		return maxim;
 	}
 
 }
